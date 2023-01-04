2132071bd7ecd9f662b695432236256db0e0116a
Gavin J. Grotegut <gavin.grotegut@gmail.com>
2023-01-04 09:17:45 -0700

    Added player list attribute to Dice (functionality to be added later)

--- Dice.py
+++ Dice.py
@@ -1,14 +1,16 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+from Player import Player
 
 
 class Dice(QtWidgets.QLabel):
-    def __init__(self, _parent: QtWidgets.QWidget, _pixmap: QtGui.QPixmap, _number: int):
+    def __init__(self, _parent: QtWidgets.QWidget, _pixmap: QtGui.QPixmap, _number: int, _player_list: list[Player]):
         super().__init__()
         self.setParent(_parent)
         self.setPixmap(_pixmap)
         self.number: int = _number
         self.locked: bool = False
         self.first_round: bool = True
+        self.player.list: list[Player] = _player_list
 
     def change_number(self, _number: int):
         self.number = _number
